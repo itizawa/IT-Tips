86f2754613976b6538e642c5358bd0f063c70709
itizawa <itizawa@weseek.co.jp>
2020-10-18 12:18:23 +0000

    adjust about card

--- components/AboutMeCard.jsx
+++ components/AboutMeCard.jsx
@@ -5,10 +5,11 @@ function AboutMeCard() {
     <div className="card p-3 side-card">
       <p className="side-card-title">IT Tips とは</p>
       <img width="100%" alt="logo" src="/tips-logov2@2x.png" />
+      <p className="text-center">
+        <strong className="text-center">若手社会人のためのIT情報専門メディア</strong>
+      </p>
       <p>
-        <strong>若手社会人のためのIT情報専門メディア</strong>
-        <br />
-        「IT Tips」は、「20代と30代の若手社会人のIT知識を底上げする」をテーマに、管理人の実務経験を通じて獲得した知見や最新の技術情報/Tipsを広くお届けするためのWebメディアです。
+        「IT Tips」は、<strong>「20代と30代の若手社会人のIT知識を底上げする」</strong>をテーマに、管理人の実務経験を通じて獲得した知見や最新の技術情報/Tipsを広くお届けするためのWebメディアです。
       </p>
     </div>
   );
